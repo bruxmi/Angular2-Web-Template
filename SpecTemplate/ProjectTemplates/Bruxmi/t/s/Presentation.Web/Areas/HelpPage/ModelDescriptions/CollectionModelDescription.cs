729dec5a2ddf962f051fb18b9d47132684e3799c
bruxmi <bruxm@DESKTOP-BT3K5CG>
2016-10-17 11:37:56 +0200

    initial create for angular2 template

---
+++ SpecTemplate/ProjectTemplates/Bruxmi/t/s/Presentation.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Presentation.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SpecTemplate/ProjectTemplates/Bruxmi/t/s/Presentation.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Presentation.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
